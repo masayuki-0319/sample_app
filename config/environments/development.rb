9bf9cda625830a9f890a0c834aa540c74d305d4c
Masayuki Onishi <omccello@gmail.com>
2020-02-20 00:54:16 +0900

    dockerによりMailHog導入

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,10 +28,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'railstutorial-yasulab.c9users.io'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp",
+    port: "1025",
+  }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.action_mailer.perform_caching = false
 
