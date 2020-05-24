92159b33314031230846f6bab6d2d3eb7d38d9fc
Masayuki Onishi <omccello@gmail.com>
2020-05-24 22:44:51 +0900

    コンテナ起動時におけるbacket自動生成を追加

--- docker/localstack/setup_s3.sh
+++ docker/localstack/setup_s3.sh
@@ -1,6 +1,1 @@
-#!/usr/bin/env bash
-
-docker-compose exec localstack aws --endpoint-url=http://localstack:4572 s3 mb s3://microposts
-
-# build check
-docker-compose exec localstack aws --endpoint-url=http://localstack:4572 s3 ls s3://
+awslocal s3 mb s3://microposts
