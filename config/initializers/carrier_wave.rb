0a30cfeee8d2723be0e76eb37547365c612a2d4b
Masayuki Onishi <omccello@gmail.com>
2020-05-26 20:29:28 +0900

    fog_credentialsを整理

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,15 +2,19 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     # Amazon S3用の設定
     :provider              => 'AWS',
+    :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
     :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    :region                => ENV['S3_REGION'],
-    :endpoint              => ENV['S3_ENDPOINT'],
-    :path_style            => !Rails.env.production?,
   }
-  config.fog_directory     =  ENV['S3_BUCKET_NAME']
+  config.fog_directory     =  ENV['S3_BUCKET']
 
-  if !Rails.env.production?
-    config.asset_host = "#{ENV['S3_ASSET_HOST']}/#{ENV['S3_BUCKET_NAME']}"
+  unless Rails.env.production?
+    config.fog_credentials.merge!(
+      {
+        :endpoint          => ENV['S3_ENDPOINT'],
+        :path_style        => true,
+      }
+    )
+    config.asset_host = "#{ENV['S3_ASSET_HOST']}/#{ENV['S3_BUCKET']}"
   end
 end
