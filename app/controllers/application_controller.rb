8a2963d2de2566bb476cdabcb78d78f6249950c6
Masayuki Onishi <omccello@gmail.com>
2020-06-22 10:37:14 +0900

    カスタム例外の残対応: rescue_from時のログ記録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from Application::NotPermittedError, with: :redirect_root_page
 
-  def redirect_root_page
+  def redirect_root_page(exception = nil)
+    Rails.logger.info "ルート URL にリダイレクト: #{exception.message}" if exception
+
     redirect_to root_url, flash: { danger: "閲覧権限がありません" }
   end
 
